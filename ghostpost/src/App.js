9c3d771148dbec8a46ca6f3bb5850dee52d9ae9c
Jessica Benson <pokeyjess72@gmail.com>
2020-10-01 12:50:48 -0400

    Voting working

--- ghostpost/src/App.js
+++ ghostpost/src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { ghostpost: [] };
+  }
+  upVoteButton(id) {
+    fetch(`http://127.0.0.1:8000/api/post/${id}/up_vote/`, { method: "GET" });
+    window.location.reload();
+  }
+
+  downVoteButton(id) {
+    fetch(`http://127.0.0.1:8000/api/post/${id}/down_vote/`, {
+      method: "GET",
+    });
+    window.location.reload();
   }
 
   componentDidMount() {
@@ -24,6 +34,13 @@ class App extends React.Component {
                 <li>{p.content}</li>
                 <p>Total votes: {p.total_votes}</p>
                 <p>Posted: {p.post_time}</p>
+                <button onClick={() => this.upVoteButton(p.id)}>
+                  Thumb's Up
+                </button>
+                &nbsp; &nbsp;
+                <button onClick={() => this.downVoteButton(p.id)}>
+                  Thumb's Down
+                </button>
               </div>
             );
           })}
